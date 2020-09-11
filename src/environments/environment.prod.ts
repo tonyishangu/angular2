6f6036962bcff97574a3bc55e4fab0daf517b230
tonyishangu <tonyishangu@gmail.com>
2020-09-11 13:16:56 +0300

    add css rule

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+//import { environment as common } from './environment';
+
+export const environment = {
+  //...common,
+  production: true,
+  apikey: 'e26634279f60a3f5b7ed624da9e989f242a46458',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apikey: 'e26634279f60a3f5b7ed624da9e989f242a46458',
+};
+
